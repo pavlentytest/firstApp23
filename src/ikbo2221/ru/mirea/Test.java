65812e86e7882151fde7a8514c5ab2ca2bd094f2
Pavel <impetus@yandex.ru>
2022-09-05 11:21:09 +0300

    Initial commit

---
+++ src/ikbo2221/ru/mirea/Test.java
@@ -0,0 +1,9 @@
+package ikbo2221.ru.mirea;
+
+public class Test {
+    public static void main(String[] args) {
+        char h = 'A';
+        String k = "HHH";
+        System.out.println("Hello");
+    }
+}
---
+++ src/practice2/Test.java
@@ -0,0 +1,9 @@
+package practice2;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        System.out.println(a+b);
+    }
+}
